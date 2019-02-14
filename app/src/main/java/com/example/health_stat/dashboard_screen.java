ee5920f2ee1d85c24ddedf32577516ee0446d853
Rajiv Kumar <rajivk@gentrack.com>
2019-02-15 00:09:33 +1300

    added screen classes

---
+++ app/src/main/java/com/example/health_stat/dashboard_screen.java
@@ -0,0 +1,13 @@
+package com.example.health_stat;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class dashboard_screen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dashboard_screen);
+    }
+}
---
+++ app/src/main/java/com/example/health_stat/profile_screen.java
@@ -0,0 +1,13 @@
+package com.example.health_stat;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class profile_screen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile_screen);
+    }
+}
