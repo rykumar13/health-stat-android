dcaff2ec8857aac2d7a2d2566794e105e94f8892
Rajiv Kumar <rajivk@gentrack.com>
2019-02-18 20:57:15 +1300

    line chart test

--- app/src/main/java/com/example/health_stat/bar_graph_drawable_test.java
+++ app/src/main/java/com/example/health_stat/bar_graph_drawable_test.java
@@ -1,12 +1,22 @@
 package com.example.health_stat;
 
+import android.graphics.Canvas;
+import android.graphics.drawable.Drawable;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.components.Description;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
 import com.github.mikephil.charting.data.BarData;
 import com.github.mikephil.charting.data.BarDataSet;
 import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
 import com.github.mikephil.charting.utils.ColorTemplate;
 
 import java.util.ArrayList;
@@ -31,5 +41,51 @@ public class bar_graph_drawable_test extends AppCompatActivity {
         BarData barData = new BarData(barDataSet);
         barChart.setData(barData);
         barDataSet.setBarShadowColor(1);
+
+        LineChart lineChart = findViewById(R.id.linechart);
+
+        List<Entry> l_data = new ArrayList<>();
+        l_data.add(new Entry(1, 45));
+        l_data.add(new Entry(2, 65));
+        l_data.add(new Entry(3, 50));
+        l_data.add(new Entry(4, 70));
+        l_data.add(new Entry(5, 90));
+        l_data.add(new Entry(6, 80));
+        l_data.add(new Entry(7, 80));
+
+        LineDataSet lineDataSet = new LineDataSet(l_data, "test");
+        LineData lineData = new LineData(lineDataSet);
+        lineChart.setData(lineData);
+        lineDataSet.setDrawFilled(true);
+        lineDataSet.setDrawValues(false);
+        Drawable drawable = ContextCompat.getDrawable(this, R.drawable.fade_linechart_background);
+        lineDataSet.setFillDrawable(drawable);
+
+        lineChart.getXAxis().setDrawGridLines(false);
+        lineChart.getAxisLeft().setDrawGridLines(false);
+        lineChart.getAxisRight().setDrawGridLines(false);
+        lineChart.getAxisRight().setDrawLabels(false);
+
+        lineChart.getLegend().setEnabled(false);
+        lineChart.setDrawBorders(false);
+
+        Description desc = new Description();
+        desc.setText("");
+        lineChart.setDescription(desc);
+
+        XAxis xAxis = lineChart.getXAxis();
+
+        xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);
+        xAxis.setDrawAxisLine(false);
+
+        YAxis yAxisLeft = lineChart.getAxisLeft();
+        yAxisLeft.setDrawGridLines(false);
+        yAxisLeft.setDrawAxisLine(false);
+
+        YAxis yAxisRight = lineChart.getAxisRight();
+        yAxisRight.setDrawGridLines(false);
+        yAxisRight.setDrawAxisLine(false);
+
+
     }
 }
--- app/src/main/java/com/example/health_stat/detail_screen.java
+++ app/src/main/java/com/example/health_stat/detail_screen.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.components.Description;
 import com.github.mikephil.charting.data.PieData;
 import com.github.mikephil.charting.data.PieDataSet;
 import com.github.mikephil.charting.data.PieEntry;
@@ -21,17 +22,21 @@ public class detail_screen extends AppCompatActivity {
         PieChart pieChart = findViewById(R.id.piechart);
         pieChart.setUsePercentValues(true);
 
-        pieChart.setHoleRadius(25f);
+        pieChart.setHoleRadius(75f);
         pieChart.setTransparentCircleRadius(20f);
 
         List<PieEntry> value = new ArrayList<>();
-        value.add(new PieEntry(400, "Achieved"));
-        value.add(new PieEntry(600, "Remaining"));
+        value.add(new PieEntry(400));
+        value.add(new PieEntry(600));
 
-        PieDataSet pieDataSet = new PieDataSet(value, "Water");
+        Description desc = new Description();
+        desc.setText("test");
+        desc.setTextSize(20f);
+        PieDataSet pieDataSet = new PieDataSet(value, "test");
 
         PieData pieData = new PieData(pieDataSet);
         pieChart.setData(pieData);
-        pieDataSet.setColor(ColorTemplate.rgb("#0095FF"));
+        pieDataSet.setColors(ColorTemplate.JOYFUL_COLORS);
+        pieDataSet.setSliceSpace(5f);
     }
 }
