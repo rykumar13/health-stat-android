cf232f3f2722c8ace835fb339f86166214de15e6
Rajiv Kumar <rajivk@gentrack.com>
2019-02-21 20:46:53 +1300

    added basic piechart to recycleview

--- app/src/main/java/com/example/health_stat/CardAdapter.java
+++ app/src/main/java/com/example/health_stat/CardAdapter.java
@@ -7,7 +7,16 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.data.PieData;
+import com.github.mikephil.charting.data.PieDataSet;
+import com.github.mikephil.charting.data.PieEntry;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class CardAdapter extends RecyclerView.Adapter<CardAdapter.CardViewHolder> {
+
 
     private String[] text;
 
@@ -26,6 +35,12 @@ public class CardAdapter extends RecyclerView.Adapter<CardAdapter.CardViewHolder
     @Override
     public void onBindViewHolder(@NonNull CardViewHolder holder, int position) {
         holder.txt_view.setText(text[position]);
+        List<PieEntry> value = new ArrayList<>();
+        value.add(new PieEntry(400));
+        value.add(new PieEntry(600));
+        PieDataSet pieDataSet = new PieDataSet(value, "test");
+        PieData pieData = new PieData(pieDataSet);
+        holder.pieChart.setData(pieData);
     }
 
     @Override
@@ -35,9 +50,12 @@ public class CardAdapter extends RecyclerView.Adapter<CardAdapter.CardViewHolder
 
     public class CardViewHolder extends RecyclerView.ViewHolder {
         TextView txt_view;
+        public PieChart pieChart;
+
         public CardViewHolder(View itemView) {
             super(itemView);
-            txt_view = (TextView) itemView.findViewById(R.id.txtTag);
+            pieChart = itemView.findViewById(R.id.piechart_card);
+            txt_view = itemView.findViewById(R.id.txtTag);
         }
 
 
